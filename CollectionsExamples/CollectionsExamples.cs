fc465e1c4f8b9d666fcb66324e309515531809a4
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-11-12 15:46:35 -0500

    Add project files.

---
+++ CollectionsExamples/CollectionsExamples.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsExamples
+{
+    public class CollectionsExamples
+    {
+        //A collection is used to group related objects.  Unlike an array, it is dynamic and can also group objects.  They can grow and shrink to accommodate any number of objects.  Collection classes are organized into namespaces and contain built in methods for processing elements within the collection.
+// A collection organizes data in a computer so that it can be used efficiently.
+//Dictionaries are used to represent conections on social websites (like Facebook), queues can be used to create task schedulers, HashSets are used in searching algorithms.
+//A collection typically includes methods to add, remove, and count objects.
+//the for and foreach statements are used to iterate through collections.
+
+//Generic Collections -- Only desired data types can be added to a generic collection
+
+        /*The namespace System.Collections.Generic has all of these generic collections inside it:
+         * List<T>
+         * Dictionary<TKey,TValue>
+         * SortedList<TKey, TValue>
+         * Stack<T>
+         * Queue<T>
+         * HashSet<T> */
+
+        /* A list of non-generic collections are:
+         * ArrayList
+         * SortedList
+         * Stack
+         *Queue
+         *Hashtable
+         *BitArray
+         */
+
+
+    }
+}
